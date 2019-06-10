214d4c05dda28bb240ddf51c451ff58ba4f7c570
MicroWin10-1947\Administrator <springboot_lhq@163.com>
2019-06-10 16:12:10 +0800

    test

--- src/main/java/com/hy/test2/Test2Maven.java
+++ src/main/java/com/hy/test2/Test2Maven.java
@@ -2,8 +2,6 @@ package com.hy.test2;
 
 public class Test2Maven {
 	public static void main(String[] args) {
-		System.out.println("123456");
-		System.out.println("lisi分支123456");
-		System.out.println("test分支");
+		System.out.println("Test2Maven");
 	}
 }
--- src/main/java/com/hy/test2/Test2Maven2.java
+++ src/main/java/com/hy/test2/Test2Maven2.java
@@ -1,7 +1,7 @@
 package com.hy.test2;
 
-public class Test2Maven1 {
+public class Test2Maven2 {
 	public static void main(String[] args) {
-		System.out.println("Test2Maven1");
+		System.out.println("Test2Maven2");
 	}
 }
