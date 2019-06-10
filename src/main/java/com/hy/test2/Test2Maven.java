d4eca909832350a2082f6111dd4d62124fcdc87d
MicroWin10-1947\Administrator <springboot_lhq@163.com>
2019-06-10 15:29:35 +0800

    test

--- src/main/java/com/hy/test2/Test2Maven.java
+++ src/main/java/com/hy/test2/Test2Maven.java
@@ -3,6 +3,6 @@ package com.hy.test2;
 public class Test2Maven {
 	public static void main(String[] args) {
 		System.out.println("123456");
-		System.out.println("abcdefghigklmnopqrstuvwxyz");
+		System.out.println("lisi分支");
 	}
 }
